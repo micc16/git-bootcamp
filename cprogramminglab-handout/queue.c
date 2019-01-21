d31f6b9a0fb21b2b871abc26802d493a6abdd1f4
Michelle C Cao <mccao@angelshark.ics.cs.cmu.edu>
2019-01-20 23:04:54 -0500

    testing

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  * Modified to store strings, 2018
@@ -36,6 +36,7 @@ void q_free(queue_t *q)
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
     free(q);
+    return;
 }
 
 /*
